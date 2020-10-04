3db658f1c2dd4bd0c1a84a9904b156111ff8c42f
Ben Follington <5009316+bfollington@users.noreply.github.com>
2020-10-04 15:21:37 +1000

    Factor out useful plotting utilities

--- src/sketches/function_plot.cljs
+++ src/sketches/function_plot.cljs
@@ -2,9 +2,13 @@
   (:require [quil.core :as q]
             [quil.middleware :as middleware]
             [sketches.palette :as pal]
-            [save-image :as f]))
+            [save-image :as f]
+            [plot :as p]))
 
-(def body (.-body js/document))
+;(def body (.-body js/document))
+;(def w (.-clientWidth body))
+;(def h (.-clientHeight body))
+
 (def w 4096)
 (def h 4096)
 (def unit (/ w 100))
@@ -23,14 +27,12 @@
   {:x (* radius (f t) (Math/cos (* winding t)))
    :y (* radius (f t) (Math/sin (* winding t)))})
 
-(defn g [r w t] (polar r w f t))
+(defn g [w t] (polar radius w f t))
 
-(defn sample [r w [min max]]
-  (map (partial g r w) (range min max 0.02)))
-
+;;;;;;;;
 
 (defn plot [idx w]
-  {:samples (sample radius w [0 (* 8 Math/PI)])
+  {:samples (p/sample #(g w %) [0 (* 8 Math/PI)] 0.02)
    :position [(* (mod idx per-row) (+ size spacing)) (* (quot idx per-row) (+ size spacing))]})
 
 (defn sketch-setup []
@@ -39,12 +41,6 @@
    :plots (map-indexed plot (range 0 Math/PI (/ Math/PI (* per-row per-row))))
    :bounds [[(- radius) radius] [(- radius) radius]]
    :size [[0 size] [0 size]]})
-
-; we want 0 -> 256 to be 0 -> 2PI and 0 -> 128 to be -1 -> 1
-(defn map-range [x [in-min in-max] [out-min out-max]]
-  (+ (/ (* (- x in-min) (- out-max out-min)) (- in-max in-min)) out-min))
-
-;(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 (defn sketch-update
   "Returns the next state to render. Receives the current state as a paramter."
@@ -63,10 +59,10 @@
       (apply q/stroke color)
       (apply q/fill color)
       (q/stroke-weight (* 0.2 unit))
-      (let [x1 (+ (map-range (:x p1) bx sx) ox px)
-            y1 (+ (map-range (:y p1) by sy) oy py)
-            x2 (+ (map-range (:x p2) bx sx) ox px)
-            y2 (+ (map-range (:y p2) by sy) oy py)]
+      (let [x1 (+ (p/map-range (:x p1) bx sx) ox px)
+            y1 (+ (p/map-range (:y p1) by sy) oy py)
+            x2 (+ (p/map-range (:x p2) bx sx) ox px)
+            y2 (+ (p/map-range (:y p2) by sy) oy py)]
         (q/line x1 y1 x2 y2)))))
 
 
